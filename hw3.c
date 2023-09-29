8ffc4d8fddf9f12a19b037afd6e5d6e78517b178
songhj8304 <127584828+songhj8304@users.noreply.github.com>
2023-09-29 19:53:45 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = i; j < 5 - 1; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < 2 * i + 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
