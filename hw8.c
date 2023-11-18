61de1c1d44bc750f1c8cc13019e3f7404dd804ca
songhj8304 <127584828+songhj8304@users.noreply.github.com>
2023-11-19 01:46:23 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void) {
+	printf("Enter 5 real numbers: ");
+	int arr[5];
+	int sum = 0, sum2 = 0;
+	double sigma = 0;
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d", &arr[i]);
+		sum += arr[i];
+	}
+	sum /= 5;
+	for (int i = 0; i < 5; i++) {
+		sum2 += pow(arr[i] - sum,2);
+	}
+	sigma = sqrt((double)sum2 / 5);
+	printf("Standard Deviation = %.3f", sigma);
+	return 0;
+}
